6ac99aa6f10ef5dbc3301590ad737b6777b99444
Milan Vasić <vasa.93@hotmail.com>
2016-04-27 15:47:02 +0200

    Show event support

--- SportEvents/SportEvents/www/scripts/global.js
+++ SportEvents/SportEvents/www/scripts/global.js
@@ -4,6 +4,16 @@
         title.html(text);
 }
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 var backBtn = document.getElementById('back-btn');
 if (backBtn != null) {
     backBtn.addEventListener('touchend', function (ev) {
