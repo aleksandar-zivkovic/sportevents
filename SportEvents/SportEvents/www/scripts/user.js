e86d7722df702a494ac0cfbca11afbf6ba36c331
Milan Vasić <vasa.93@hotmail.com>
2016-04-27 16:54:45 +0200

    Commit

---
+++ SportEvents/SportEvents/www/scripts/user.js
@@ -0,0 +1,19 @@
+﻿var title = document.getElementById('action-bar-title-text');
+title.className += " action-bar-title-small";
+
+$.ajax({
+    type: 'GET',
+    url: 'http://vasic.ddns.net/events/getuserbyid',
+    data: "id=" + getParameterByName("id", window.location),
+    success: parseUser,
+    error: function (xhr, status, error) {
+        alert(error.message);
+    }
+});
+
+function parseUser(data) {
+    //parse JSON data
+    event = JSON.parse(data);
+
+    setTitle(event.name);
+}
